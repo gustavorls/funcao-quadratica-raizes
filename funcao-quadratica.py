e94f2c7feed82375ff31834bd4c9c47a4aa4d1eb
Gustavo Rocha <59335090+yokiitos@users.noreply.github.com>
2021-06-09 22:44:32 -0300

    complete code added

---
+++ funcao-quadratica.py
@@ -0,0 +1,18 @@
+import cmath
+
+a = int(input("Insira o valor de a: "))
+b = int(input("Insira o valor de b: "))
+c = int(input("Insira o valor de c: "))
+
+delta = (b ** 2) - (4 * a * c)
+bhaskaraMais = (-b + cmath.sqrt(delta)) / (2 * a)
+bhaskaraMenos = (-b - cmath.sqrt(delta)) / (2 * a)
+bhaskaraZero = bhaskaraMais + bhaskaraMenos
+
+if delta < 0:
+    print("Não existem raizes reais")
+elif delta == 0:
+    print("Há uma única raiz real de valor: x=", int(bhaskaraZero))
+elif delta > 0:
+    print("Existem duas raízes reais de valores: x1=",
+          int(bhaskaraMais), "e x2=", int(bhaskaraMenos))
